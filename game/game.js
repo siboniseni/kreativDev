23ae524dbbea99133f55f4cbe02f88b07b303ec5
Siboniseni Kasa <122732898+siboniseni@users.noreply.github.com>
2023-08-05 12:09:45 +0200

    playRound Function updated

--- game/game.js
+++ game/game.js
@@ -13,18 +13,38 @@ function computerPlay() {
 
 // Function to determine the result of a single round
 function playRound(playerSelection, computerSelection) {
+    const winMessages = [
+        "You're on fire! You roasted that Rock!",
+        "Rock and Roll! You crushed that Scissors!",
+        "Scissors sliced and diced! You win!",
+    ];
+
+    const loseMessages = [
+        "Oh no! The computer's Paper covered your Rock.",
+        "Tough luck! Computer's Rock smashed your Scissors.",
+        "Snip snip! Computer's Scissors got the best of you.",
+    ];
+
+    const tieMessages = [
+        "Great minds think alike! It's a Tie!",
+        "It's a draw! Nobody wins this round.",
+        "A tie! No winners, no losers.",
+    ];
+
     if (playerSelection == "paper" && computerSelection == "rock") {
-        return "You Win! Paper beats Rock";
+        return "You Win! " + winMessages[Math.floor(Math.random() * winMessages.length)];
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        return "You Win! Rock beats Scissors";
+        return "You Win! " + winMessages[Math.floor(Math.random() * winMessages.length)];
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return "You Win! Scissors beats Paper";
+        return "You Win! " + winMessages[Math.floor(Math.random() * winMessages.length)];
     } else if (playerSelection == computerSelection) {
-        return "It's a Tie!";
+        return tieMessages[Math.floor(Math.random() * tieMessages.length)];
     } else {
-        return "You Lose! " + computerSelection + " beats " + playerSelection;
+        return "You Lose! " + loseMessages[Math.floor(Math.random() * loseMessages.length)];
     }
 }
+
+// The rest of your code remains unchanged...
 
 // Main function to run the game
 function game() {
