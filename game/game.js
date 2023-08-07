6a1285bff88b878345309620bd5f910695f3ddc8
Siboniseni Kasa <122732898+siboniseni@users.noreply.github.com>
2023-08-07 11:29:40 +0200

    when user cancels the game

--- game/game.js
+++ game/game.js
@@ -1,84 +1,101 @@
-// Function to randomly select 'rock', 'paper', or 'scissors' for the computer
 function computerPlay() {
-    const choice = Math.floor(Math.random() * 3);
+  
+  const choice = Math.floor(Math.random() * 3);
 
-    if (choice === 0) {
-        return 'rock';
-    } else if (choice === 1) {
-        return 'paper';
-    } else {
-        return 'scissors';
-    }
+  if (choice === 0) {
+    return 'rock';
+  } else if (choice === 1) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
 }
 
-// Function to determine the result of a single round
 function playRound(playerSelection, computerSelection) {
-    const winMessages = [
-        "You're on fire! You roasted that Rock!",
-        "Rock and Roll! You crushed that Scissors!",
-        "Scissors sliced and diced! You win!",
-    ];
+  const winMessages = [
+      "You're on fire! You roasted that Rock!",
+      "Rock and Roll! You crushed that Scissors!",
+      "Scissors sliced and diced! You win!",
+  ];
 
-    const loseMessages = [
-        "Oh no! The computer's Paper covered your Rock.",
-        "Tough luck! Computer's Rock smashed your Scissors.",
-        "Snip snip! Computer's Scissors got the best of you.",
-    ];
+  const loseMessages = [
+      "Oh no! The computer's Paper covered your Rock.",
+      "Tough luck! Computer's Rock smashed your Scissors.",
+      "Snip snip! Computer's Scissors got the best of you.",
+  ];
 
-    const tieMessages = [
-        "Great minds think alike! It's a Tie!",
-        "It's a draw! Nobody wins this round.",
-        "A tie! No winners, no losers.",
-    ];
+  const tieMessages = [
+      "Great minds think alike! It's a Tie!",
+      "It's a draw! Nobody wins this round.",
+      "A tie! No winners, no losers.",
+  ];
 
-    if (playerSelection == "paper" && computerSelection == "rock") {
-        return "You Win! " + winMessages[Math.floor(Math.random() * winMessages.length)];
-    } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        return "You Win! " + winMessages[Math.floor(Math.random() * winMessages.length)];
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return "You Win! " + winMessages[Math.floor(Math.random() * winMessages.length)];
-    } else if (playerSelection == computerSelection) {
-        return tieMessages[Math.floor(Math.random() * tieMessages.length)];
-    } else {
-        return "You Lose! " + loseMessages[Math.floor(Math.random() * loseMessages.length)];
-    }
+  if (playerSelection == "paper" && computerSelection == "rock") {
+      return "You Win! " + winMessages[Math.floor(Math.random() * winMessages.length)];
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+      return "You Win! " + winMessages[Math.floor(Math.random() * winMessages.length)];
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+      return "You Win! " + winMessages[Math.floor(Math.random() * winMessages.length)];
+  } else if (playerSelection == computerSelection) {
+      return tieMessages[Math.floor(Math.random() * tieMessages.length)];
+  } else {
+      return "You Lose! " + loseMessages[Math.floor(Math.random() * loseMessages.length)];
+  }
 }
 
-// The rest of your code remains unchanged...
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
+  let playerScore = 0;
+  let computerScore = 0;
 
-    for (let i = 1; i < 6; i++) {
-        
-        let playerSelection = prompt("Round " + i + "\nEnter your choice: ").toLowerCase();
+  for (let i = 1; i < 6; i++) {
+    let playerSelection = prompt("Round " + i + "\nEnter your choice: ");
+    
+    // Check if the user canceled
+    if (playerSelection === null) {
+      console.log(`Final Scores: Player Score: ${playerScore} - Computer Score: ${computerScore}`);
+      console.log("You quit the game. Goodbye!");
+      return;
+    }
+    
+    playerSelection = playerSelection.toLowerCase();
 
-        // Input validation loop
-        while (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
-            playerSelection = prompt("Invalid choice. Please enter 'rock', 'paper', or 'scissors': ").toLowerCase();
-        }
-
-        const computerSelection = computerPlay();
-
-        // Determine the result of the round and update scores
-        const result = playRound(playerSelection, computerSelection);
-        console.log(result);
-
-        if (result.startsWith("You Win!")) {
-            playerScore++;
-        } else if (result.startsWith("You Lose!")) {
-            computerScore++;
-        }
+    // Input validation loop
+    while (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
+      playerSelection = prompt("Invalid choice. Please enter 'rock', 'paper', or 'scissors': ");
+      
+      // Check if the user canceled during input validation
+      if (playerSelection === null) {
+        console.log(`Final Scores: Player Score: ${playerScore} - Computer Score: ${computerScore}`);
+        console.log("You quit the game. Goodbye!");
+        return;
+      }
+      
+      playerSelection = playerSelection.toLowerCase();
     }
 
-    // Determine the winner of the game based on scores
-    if (playerScore > computerScore) {
-        console.log("You win the game!");
-    } else if (playerScore < computerScore) {
-        console.log("Computer wins the game!");
-    } else {
-        console.log("It's a tie game!");
+    const computerSelection = computerPlay();
+
+    // Determine the result of the round and update scores
+    const result = playRound(playerSelection, computerSelection);
+    console.log(result);
+
+    if (result.startsWith("You Win!")) {
+      playerScore++;
+    } else if (result.startsWith("You Lose!")) {
+      computerScore++;
     }
+
+    console.log(`Player Score: ${playerScore} - Computer Score: ${computerScore}`);
+  }
+
+  // Determine the winner of the game based on scores
+  if (playerScore > computerScore) {
+    console.log("You win the game!");
+  } else if (playerScore < computerScore) {
+    console.log("Computer wins the game!");
+  } else {
+    console.log("It's a tie game!");
+  }
 }
 
 game();
